16ccb7a3395f66224642d7bb6dfe19f9c6942641
Dario Araya <dararayag@gmail.com>
2022-04-20 21:54:31 -0400

    refactorizando

---
+++ routes/eroskiRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getAll, findForName, findByCat, sortBy } = require('../controllers/eroskiControllers')
+
+//Mostrar todos los productos de la categoria inicial
+router.get('/all-products', getAll);
+
+//filtrar por nombre
+router.post('/find-for-name', findForName);
+
+//filtrar por categoria
+router.post('/filter-by-cat', findByCat);
+
+//ordenar por
+router.post('/sort-by', sortBy);
+
+module.exports = router;
