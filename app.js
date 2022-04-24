771e206e7506fa04108b915b5712b91f0e675557
Dario Araya <dararayag@gmail.com>
2022-04-24 19:05:47 -0400

    preparando deploy

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const conectando = require('./src/mysql_connector.js');
+const path = require("path");
+const conectando = require("./src/mysql_connector.js");
 const ejsMate = require("ejs-mate");
-const eroski = require('./routes/eroskiRoutes');
+const cors = require("cors");
+const eroski = require("./routes/eroskiRoutes");
 
-app.engine('ejs',ejsMate)
-app.set('view engine', 'ejs');
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 
-conectando.connect((err)=>{
-    if(err) throw err;
-    console.log("Conectado a la base");
+conectando.connect((err) => {
+  if (err) throw err;
+  console.log("Conectado a la base");
 });
+
+const whiteList = ["http://localhost:3000", "https://api-eroski.herokuapp.com"];
+
+app.use(cors({ origin: whiteList }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', eroski);
+app.use("/", eroski);
 
 //Iniciar servidor
 const port = process.env.PORT || 4000;
-app.listen(port, ()=>{
-    console.log(`Serving on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
 
 //keep alive
 setInterval(function () {
-    conectando.query('SELECT 1');
+  conectando.query("SELECT 1");
 }, 4000);
--- controllers/eroskiControllers.js
+++ controllers/eroskiControllers.js
@@ -1,90 +1,91 @@
-
-const conectando = require('../src/mysql_connector');
+const conectando = require("../src/mysql_connector");
 
 //Querys
-const ordenarPor =['nombreA','nombreD','precioA','precioD'];
+const ordenarPor = ["nombreA", "nombreD", "precioA", "precioD"];
 
-let query =  `SELECT * FROM product where category = `;
-let query2 =  'SELECT * FROM category';
-let query3= 'SELECT * from product where name LIKE'
+let query = `SELECT * FROM product where category = `;
+let query2 = "SELECT * FROM category";
+let query3 = "SELECT * from product where name LIKE";
 
 //Mostrar todos los productos de la categoria inicial
-module.exports.getAll = async(req,res,next)=>{
-    let id = 1;  
-    try {
-        await conectando.query(`${query} ${id}`,async(err, products) => {
-            if(err) res.json({status:500, err});
-            conectando.query(query2,(err,categories)=>{
-            if(err) res.json({status:500, err});
-            res.json({products,categories,ordenarPor})    
-             });
-           });
-    } catch (error) {
-        console.log(error)
-    }
+module.exports.getAll = async (req, res, next) => {
+  let id = 1;
+  try {
+    await conectando.query(`${query} ${id}`, async (err, products) => {
+      if (err) res.json({ status: 500, err });
+      conectando.query(query2, (err, categories) => {
+        if (err) res.json({ status: 500, err });
+        res.json({ products, categories, ordenarPor });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //filtrar por nombre
-module.exports.findForName= async(req,res,next)=>{
-    let name = req.body.name;
-        try {
-            if(name.indexOf('"')!=-1){
-                name = name.replace(/['"]+/g, '')
-             }
-                await conectando.query(`${query3} '%${name}%'`,(err, products)=>{
-                if(err) res.json({status:500, err});
-                conectando.query(query2,(err,categories)=>{
-                if(err) res.json({status:500, err});
-                res.json({products,categories,ordenarPor})
-                });
-             }); 
-             
-        } catch (err) {
-            console.log(err);
-          
-        }
+module.exports.findForName = async (req, res, next) => {
+  let name = req.body.name;
+  let id = req.body.id;
+  try {
+    if (name.indexOf('"') != -1) {
+      name = name.replace(/['"]+/g, "");
+    }
+    await conectando.query(
+      `${query3} '%${name}%' AND category=${id}`,
+      (err, products) => {
+        if (err) res.json({ status: 500, err });
+        conectando.query(query2, (err, categories) => {
+          if (err) res.json({ status: 500, err });
+          res.json({ products, categories, ordenarPor });
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //filtrar por categoria
-module.exports.findByCat=async(req,res,next)=>{
-    let id = req.body.id;
-    try {
-        await conectando.query(`${query} '${id}'`,(err, products)=>{
-            if(err) res.json({status:500, err});
-            conectando.query(query2,(err,categories)=>{
-            if(err) res.json({status:500, err});
-            res.json({products,categories,ordenarPor}) 
-            })
-        });  
-    }  catch (err) {
-        console.log(err);   
-    }    
+module.exports.findByCat = async (req, res, next) => {
+  let id = req.body.id;
+  try {
+    await conectando.query(`${query} '${id}'`, (err, products) => {
+      if (err) res.json({ status: 500, err });
+      conectando.query(query2, (err, categories) => {
+        if (err) res.json({ status: 500, err });
+        res.json({ products, categories, ordenarPor });
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //ordenar por
-module.exports.sortBy=async(req,res,next)=>{
-    let sortBy = req.body.sortBy;
-    let id = req.body.id;
-    try {
-        if(sortBy==='nombreA'){
-            var query3 = `${query} '${id}' ORDER BY name`;
-        }else if(sortBy==='nombreD'){
-            var query3 = `${query} '${id}' ORDER BY name DESC`;
-        }else if(sortBy==='precioA'){
-            var query3 = `${query} '${id}' ORDER BY price DESC`;
-        }else if(sortBy==='precioD'){
-            var query3 = `${query} '${id}' ORDER BY price`;
-        }else{
-            var query3 = `${query} '${id}'`;
-        }     
-        conectando.query(query3,async(err,products)=>{
-            if(err) res.json({status:500, err});
-            await conectando.query(query2,(err,categories)=>{
-            if(err) res.json({status:500, err});
-            res.json({products,categories,ordenarPor})        
-                })
-            });
-    } catch (err) {
-        console.log(err);  
-    }  
+module.exports.sortBy = async (req, res, next) => {
+  let sortBy = req.body.sortBy;
+  let id = req.body.id;
+  try {
+    if (sortBy === "nombreA") {
+      var query3 = `${query} '${id}' ORDER BY name`;
+    } else if (sortBy === "nombreD") {
+      var query3 = `${query} '${id}' ORDER BY name DESC`;
+    } else if (sortBy === "precioA") {
+      var query3 = `${query} '${id}' ORDER BY price DESC`;
+    } else if (sortBy === "precioD") {
+      var query3 = `${query} '${id}' ORDER BY price`;
+    } else {
+      var query3 = `${query} '${id}'`;
+    }
+    conectando.query(query3, async (err, products) => {
+      if (err) res.json({ status: 500, err });
+      await conectando.query(query2, (err, categories) => {
+        if (err) res.json({ status: 500, err });
+        res.json({ products, categories, ordenarPor });
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
